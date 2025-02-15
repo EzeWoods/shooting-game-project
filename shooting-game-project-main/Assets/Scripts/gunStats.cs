803292224777b3c2bd1376e6cb943bcc091ec31d
Caleb O'Connell <cmoconnell@student.fullsail.edu>
2025-02-14 21:03:58 -0500

    Updated gunStats Added predefined gun models (AK-47, M4A1, Skorpion VZ, UMP-45). Improved ammo system with max capacity and reserve tracking. Implemented hit particle effects for shooting. Integrated shooting sound effects with volume control.

--- shooting-game-project-main/Assets/Scripts/gunStats.cs
+++ shooting-game-project-main/Assets/Scripts/gunStats.cs
@@ -1,21 +1,53 @@
 using UnityEngine;
 
-[CreateAssetMenu]
-
-public class gunStats : ScriptableObject
+[CreateAssetMenu(fileName = "NewGun", menuName = "Weapons/GunStats")]
+public class GunStats : ScriptableObject
 {
+    [Header("Gun Model & Info")]
     public GameObject model;
-    public string name;
+    public string gunName;
+
+    [Header("Gun Stats")]
     public int shootDamage;
-    public int shootDistance;
+    public int shootDist;
     public float shootRate;
     public float reloadTime;
-    public int ammoCurrent;
-    public int ammoStored;
+
+    [Header("Ammo Stats")]
+    [SerializeField] private int ammoCurrent;
+    [SerializeField] private int ammoStored;
     public int ammoMax;
     public int ammoMaxStored;
 
+    [Header("Gun Effects & Sounds")]
     public ParticleSystem hitEffect;
     public AudioClip[] shootSound;
     public float shootSoundVolume;
+
+    [Header("Fire Mode")]
+    public enum FireMode { SemiAuto, FullAuto, Burst }
+    public FireMode fireMode;
+
+    // Getters & Setters to manage ammo
+    public int GetAmmoCurrent() => ammoCurrent;
+    public int GetAmmoStored() => ammoStored;
+
+    public void UseAmmo(int amount)
+    {
+        ammoCurrent = Mathf.Max(0, ammoCurrent - amount);
+    }
+
+    public void Reload()
+    {
+        int ammoNeeded = ammoMax - ammoCurrent;
+        int ammoToReload = Mathf.Min(ammoNeeded, ammoStored);
+
+        ammoCurrent += ammoToReload;
+        ammoStored -= ammoToReload;
+    }
+
+    public void AddAmmo(int amount)
+    {
+        ammoStored = Mathf.Min(ammoMaxStored, ammoStored + amount);
+    }
 }
